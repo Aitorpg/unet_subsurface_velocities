d102f5108627e4356dcc26b2c4b003096ba8360a
Vant <aitor.perez@lfda.de>
2025-09-18 15:17:04 +0200

    Chore: uploading training files and additional functions

---
+++ helper_functions.py
@@ -0,0 +1,277 @@
+from netCDF4 import Dataset
+import numpy as np
+import xarray as xr
+import calendar
+from unet import UNet_nobatchnorm
+import torch
+import os
+import pandas as pd
+from datetime import datetime, timedelta
+from sklearn.metrics import r2_score
+import warnings
+from scipy.linalg import eigh_tridiagonal
+import matplotlib.pyplot as plt
+import seaborn as sns
+import colorcet as cc
+import gsw
+
+def list_files(uvel=False, vvel=False, temp=False):
+    """
+    This function returns one whole year (2007) of daily data as the training set 
+    and 4 months (January, April, July, of next year (2008) as the testing set
+
+    Options:
+
+    infer zonal velocity field (uvel=True)
+    infer meridional velocity field (vvel=True)
+    infer temperature field (temp=True)
+
+    The paths and file names should be adapted to the users dataset
+    """
+    
+    ssh_path_file = "" # Path to ssh data files
+    temp_path_file = "" # Path to temprature data files
+    vvel_path_file = "" # Path to meridional velocity data files
+    uvel_path_file = "" # Path to zonal velocity data files
+    
+
+    train_files = []
+    test_files = []
+
+    # TRAIN SET
+    for year in range(2007, 2008):  # Covers 2007
+        for month in range(1, 13):  # Months from 1 to 12
+            num_days = calendar.monthrange(year, month)[1]
+            for day in range(1, num_days + 1): # Looping up to the correct number of days in the month
+                day_str = f"{day:02d}"  # Format day as two digits (01, 02, ..., 31)
+                month_str = f"{month:02d}"  # Format month as two digits (01, 02, ..., 12)
+            
+                ssh_filename = f"{ssh_path_file}SSH_t.cfc11.{year}{month_str}{day_str}.nc"
+                temp_filename = f"{temp_path_file}TEMP_t.cfc11.{year}{month_str}{day_str}.nc"
+                
+                vvel_filename = f"{vvel_path_file}VVEL_t.cfc11.{year}{month_str}{day_str}.nc"
+                uvel_filename = f"{uvel_path_file}UVEL_t.cfc11.{year}{month_str}{day_str}.nc"
+                
+                if uvel and vvel:
+                    train_files.append(ssh_filename)
+                    train_files.append(temp_filename)
+                    train_files.append(uvel_filename)
+                    train_files.append(vvel_filename)
+                else:
+                    if uvel:
+                        output_filename = uvel_filename  
+                    elif vvel:
+                        output_filename = vvel_filename
+                    elif temp:
+                        output_filename = temp_filename
+
+                    train_files.append(ssh_filename)
+                    train_files.append(temp_filename)
+                    train_files.append(output_filename)
+
+    # TEST SET
+    for year in range(2008, 2009):
+        for month in range(1, 13, 3):
+            num_days = calendar.monthrange(year, month)[1]
+            for day in range(1, num_days + 1):  # Looping up to the correct number of days in the month
+                day_str = f"{day:02d}"  # Format day as two digits (01, 02, ..., 31)
+                month_str = f"{month:02d}"  # Format month as two digits (01, 02, ..., 12)
+            
+                ssh_filename = f"/work/uo0780/u302044/POPCFC/YEARLY/SSH/SSH_t.cfc11.{year}{month_str}{day_str}.nc"
+                temp_filename = f"/work/uo0780/u302044/POPCFC/YEARLY/TEMP/TEMP_t.cfc11.{year}{month_str}{day_str}.nc"
+                ehf_filename = f"/work/uo0780/u302044/POPCFC/YEARLY/EDDYFLUXES/UVPTEMPP_{year}{month_str}{day_str}.nc"
+                vvel_filename = f"/work/uo0780/u241194/u241194/POPCFC/DATANC/VVEL_t.cfc11.{year}{month_str}{day_str}.nc"
+                uvel_filename = f"/work/uo0780/u241194/u241194/POPCFC/DATANC/UVEL_t.cfc11.{year}{month_str}{day_str}.nc"
+                if uvel and vvel:
+                    test_files.append(ssh_filename)
+                    test_files.append(temp_filename)
+                    test_files.append(uvel_filename)
+                    test_files.append(vvel_filename)
+                else:
+                    if uvel:
+                        output_filename = uvel_filename  
+                    elif vvel:
+                        output_filename = vvel_filename
+                    elif temp:
+                        output_filename = temp_filename
+                    else:
+                        output_filename = ehf_filename
+                
+                    test_files.append(ssh_filename)
+                    test_files.append(temp_filename)
+                    test_files.append(output_filename)
+    return train_files, test_files
+
+def load_data_from_nc_as_lists(vvel=False, uvel=False, temp=False):
+    """
+    Loading the train and test sets as lists. For each file in the train and test lists 
+    (e.g., ssh_1.nc, temp_1.nc, edf_1.nc, ssh_2.nc, temp_2.nc, etc.), the values ssh, temp, and ehf          
+    were appended in that specific order.
+    """
+    train_files, test_files = list_files(vvel=vvel, uvel=uvel, temp=temp)
+    nctrains = [xr.open_dataset(f) for f in train_files]
+    nctest = [xr.open_dataset(f) for f in test_files]
+    return nctrains, nctest
+
+# Load all data into CPU memory. 
+def preload_data(nctrains, total_records, N_inp, N_out, var_input_names, depth_levels, var_output, min_height, max_height, min_width, max_width, num_recs=1, jump_first_depth_level=False):
+    all_input_data = np.zeros((total_records, N_inp, max_height-min_height, max_width-min_width))*np.nan
+    all_output_data = np.zeros((total_records, N_out, max_height-min_height, max_width-min_width))*np.nan
+    current_index = 0
+    # Assuming nctrains is a list of the .nc files: ['ssh_1.nc', 'sst_1.nc', 'edf_1.nc', 'ssh_2.nc', 'sst_2.nc', 'edf_2.nc']
+    for ncindex in range(0, len(nctrains), 3):  # Increment by 3 to process every 3 files as a group
+        # Get the 3 files (e.g., ssh, sst, edf) for this group
+        ssh_data = nctrains[ncindex]      # First file of the group (ssh)
+        temp_data = nctrains[ncindex + 1]  # Second file of the group (sst)
+        output_data = nctrains[ncindex + 2]  # Third file of the group (edf)
+
+        # Define the record slice for the current group
+        rec_slice = slice(current_index, current_index + num_recs)
+
+        # Process input variables (e.g., ssh, sst, edf)
+        for ind, var_name in enumerate(var_input_names):
+            # Read data from each file in the group for the corresponding variable
+            data = ssh_data if ind == 0 else temp_data
+            data_slice = data[var_name].isel(i_index=slice(min_width,max_width), j_index=slice(min_height,max_height)) if ind == 0 else data[var_name].isel(i_index=slice(min_width,max_width), j_index=slice(min_height,max_height))[0, :, :]
+            land_sea_mask = np.isnan(data_slice) # Land Sea Mask
+
+            data_slice = np.nan_to_num(data_slice, nan=0.0)
+            # Get the actual dimensions of the data_slice
+            slice_height, slice_width = data_slice.shape[-2], data_slice.shape[-1]
+            
+            # Assign the data to the all_input_data array
+            all_input_data[rec_slice, ind, :slice_height, :slice_width] = data_slice
+
+        # Process output variables (if any)
+        for depth_level in range(depth_levels):
+            var_depth_level = depth_level + 1 if jump_first_depth_level else depth_level
+            data_slice = output_data[var_output].isel(i_index=slice(min_width,max_width), j_index=slice(min_height,max_height))[var_depth_level, :, :]
+            # Get the actual dimensions of the data_slice
+            slice_height, slice_width = data_slice.shape[-2], data_slice.shape[-1]
+            
+            mask_ehf = land_sea_mask | (data_slice == 99999)  # Mask for NaNs from SSH and 99999 in EDDYFLUXES
+            
+            data_slice = data_slice.where(~mask_ehf, 0)
+            if var_output == "TEMP":
+                data_slice = np.nan_to_num(data_slice, nan=0.0)
+            # Assign the data to the all_output_data array
+            all_output_data[rec_slice, depth_level, :slice_height, :slice_width] = data_slice
+
+        # Update current_index after processing the group of 3 files
+        current_index += num_recs
+
+    return all_input_data, all_output_data
+
+# pull a batch of data from preloaded memory. To be called during training
+def loaddata_preloaded_train(index, batch_size, all_input_data, all_output_data, lim, width):
+    rec_slice = slice(index, index + batch_size)
+    yslice = slice(0, lim) #It's important to start from 0 here: in preload_data, we have padded the data in the last elements when the input data has smaller dimensions. 
+    xslice = slice(0, width)
+    # print('rec_slice is:')
+    # print(rec_slice)
+    # print('mean of squared values of loaded input data:')
+    # print("{0:0.32f}".format(np.nanmean(all_input_data[rec_slice, :, yslice, xslice]**2)))
+    return (all_input_data[rec_slice, :, yslice, xslice], 
+            all_output_data[rec_slice, :, yslice, xslice])
+    
+#pull data from preloaded memory for testing. The difference from this and loaddata_preloaded_train is that
+#we load test data as one single batch for testing. 
+def loaddata_preloaded_test(all_input_data, all_output_data, lim, width):
+    #rec_slice = slice(index, index + batch_size)
+    yslice = slice(0, lim)
+    xslice = slice(0, width)
+    # print('rec_slice is:')
+    # print(rec_slice)
+    # print('mean of squared values of loaded input data:')
+    # print("{0:0.32f}".format(np.nanmean(all_input_data[rec_slice, :, yslice, xslice]**2)))
+    return (all_input_data[:, :, yslice, xslice], 
+            all_output_data[:, :, yslice, xslice])
+
+def convert_to_ij(lon_min, lon_max, lat_min, lat_max):
+    # Load dataset
+    ds = xr.open_dataset("/home/u/u302044/dataset/UVEL_t.cfc11.20041109.nc")
+
+    # Get the 2D coordinate arrays
+    lon_2d = ds.U_LON_2D.values
+    lat_2d = ds.U_LAT_2D.values
+
+    # Initialize mask of all False
+    mask = np.ones(lon_2d.shape, dtype=bool)
+    
+    # Apply bounding box mask
+    mask &= (lon_2d >= lon_min) & (lon_2d <= lon_max)
+    mask &= (lat_2d >= lat_min) & (lat_2d <= lat_max)
+
+    # Find indices where mask is True
+    indices = np.argwhere(mask)
+
+    if indices.size == 0:
+        raise ValueError("No grid points found within the given bounding box.")
+
+    # Get bounding box of the selected indices
+    i_min = np.min(indices[:, 0])
+    i_max = np.max(indices[:, 0])
+    j_min = np.min(indices[:, 1])
+    j_max = np.max(indices[:, 1])
+
+    return i_min, i_max, j_min, j_max
+
+def nn_pred(checkpoint, N_inp, N_out, Nbase, day, min_width, max_width, min_height, max_height, depth_level=None, model=None):
+    # Instantiate the model with the correct parameters
+    model = UNet_nobatchnorm(N_inp, N_out, bilinear=True, Nbase=Nbase).cuda() if not model else model # Move to GPU
+
+
+    # Load model weights
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()  # Set model to evaluation mode
+
+    ssh_file = f"/work/uo0780/u241194/u241194/POPCFC/DATANC/SSH_t.cfc11.{day}.nc"
+    sst_file = f"/work/uo0780/u241194/u241194/POPCFC/DATANC/TEMP_t.cfc11.{day}.nc"
+
+    # Open the SSH and SST data using xarray
+    ssh_data = xr.open_dataset(ssh_file)
+    sst_data = xr.open_dataset(sst_file)
+
+    # Extract SSH and SST variables (replace with the actual variable names)
+    ssh = ssh_data['SSH']
+    sst = sst_data['TEMP']  # Assuming 'TEMP' contains the SST data
+
+    # Select region
+    ssh_slice = ssh.isel(i_index=slice(min_width, max_width), j_index=slice(min_height, max_height))
+    sst_slice = sst.isel(i_index=slice(min_width, max_width), j_index=slice(min_height, max_height))[0, :, :]
+    
+    land_sea_mask = np.isnan(ssh_slice) # Land Sea Mask
+
+    # Stack SSH and SST into one input tensor (2 channels)
+    input_data = np.stack([ssh_slice.values, sst_slice.values], axis=0)
+    
+    land_sea_mask = np.isnan(ssh_slice) # Land Sea Mask
+    
+    # All NAN values to 0
+    input_data = np.nan_to_num(input_data, nan=0.0)
+    # Normalize the data if required by your model (depending on the training process)
+    input_data = input_data - checkpoint['mean_input'][None, :, None, None]
+    input_data=input_data/np.sqrt(checkpoint['var_input'][None, :, None, None])
+    
+    # Convert the input data to a PyTorch tensor and add batch dimension
+    input_tensor = torch.tensor(input_data, dtype=torch.float32).cuda()  # Add batch dimension and move to GPU
+    
+    # Step 4: Make predictions using the model
+    with torch.no_grad():
+        prediction = model(input_tensor)  # Perform inference
+    
+    prediction = prediction.cpu() * np.sqrt(checkpoint['var_output'][None, :, None, None]) + checkpoint['mean_output'][None, :, None, None]
+    
+    predicted_output = prediction.squeeze().cpu().numpy()  # Remove batch dimension and move to CPU
+    if not depth_level:
+        #mask_ehf = land_sea_mask[None, :, :] | (predicted_output == 99999)  # Mask for NaNs from SSH and 99999 in EDDYFLUXES
+        #predicted_output = np.where(~mask_ehf, predicted_output, np.nan)
+        return predicted_output
+    # Extract the desired depth level (the 0th depth level in this case)
+    predicted_depth_level = predicted_output[depth_level, :, :] if N_out > 1 else predicted_output 
+
+    mask_ehf = land_sea_mask | (predicted_depth_level == 99999)  # Mask for NaNs from SSH and 99999 in EDDYFLUXES
+
+    predicted_depth_level = np.where(~mask_ehf, predicted_depth_level, np.nan)
+
+    return predicted_depth_level
---
+++ train_unet.py
@@ -0,0 +1,256 @@
+print("U-Net Training started!")
+# A code with the U-Net architecture and learning rate annealing. 
+# The code trains the UNet and saves the checkpoint.
+
+import numpy as np
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+from tqdm import tqdm
+import torch.nn.functional as F
+from sklearn.metrics import r2_score as R2
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.metrics import r2_score as r2
+from copy import deepcopy
+import utils
+from unet import UNet_nobatchnorm
+from scipy.stats import pearsonr
+import helper_functions as hf
+torch.cuda.set_device(0)
+import os
+import psutil
+
+######## PARAMETERS ###########
+
+maxEpochs = 1500 # Training epochs
+Nbase = 64 # depth of the UNet
+
+depth_levels = 42 # depth levels of the output data (output channels) 
+
+# select the region
+lon_min = -75
+lon_max = -55
+lat_min = -65
+lat_max = -55
+
+var_ssh = 'SSH' # variable name on nc file for ssh 
+var_temp = 'TEMP' # variable name on nc file for temperature (in our case sst is the depth level 1 of the temperature file)
+var_output = 'VVEL' # variable name on nc file for meridional velocity field (change in case of infering another variable such as the zonal velocity for example)
+
+###############################
+
+def log_memory(msg=""):
+    """
+    function used to monitor the memory used. 
+    In this script is used only before and after 
+    loading the dataset for the training and testing
+    """
+    process = psutil.Process(os.getpid())
+    mem = process.memory_info().rss / (1024 ** 3)  # in GB
+    print(f"[MEMORY] {msg} -- {mem:.2f} GB used")
+
+def run_model(var_input_names, var_output_names, save_fn_prefix, N_inp, N_out, mean_input, mean_output, var_input, var_output):
+    #function to move data from CPU to GPU memory
+    def totorch(x):
+        return torch.tensor(x, dtype = torch.float).cuda()
+    #main definition of the UNet. This is a standard UNet unit with no batchnorm layers. Nbase controls how deep the network is. You can
+    #investigate the effects of Nbase, or other parameters, such as the way pooling is done for the downsampling layers in UNet_nobatchnorm.
+    model = UNet_nobatchnorm(N_inp, N_out, bilinear = True, Nbase = Nbase).cuda()
+
+    input = torch.randn(1,N_inp,max_height-min_height,max_width-min_width).to(device) #a fake piece of data with the same sizes as a snapshot of the training data. This is just
+    
+    #to estimate the size of the UNet. 
+    output = model(input)
+    print('Model has ', utils.nparams(model)/1e6, ' million params')
+
+    inp_test, out_test = hf.loaddata_preloaded_test(all_test_input, all_test_output, max_height-min_height, max_width-min_width)
+    print('shapes of input and output TEST data:')
+    print(inp_test.shape, out_test.shape)
+    with torch.no_grad():
+        inp_test = totorch(inp_test)
+
+    Tcycle = 10 #Cycle for the learning rate annealing.
+    criterion_train  = nn.L1Loss()
+    optim = torch.optim.AdamW(model.parameters(), lr=lr0, betas=(0.9, 0.999), eps=1e-8, weight_decay=1e-5*100) #weight_decay could be adjusted
+
+    r2_test = np.zeros(maxEpochs)
+    train_loss_per_epoch = np.zeros(maxEpochs)
+    epochmin = []
+    maxr2l = []
+    learn = np.zeros(maxEpochs)
+    minloss = 1000
+    maxR2 = -1000 #Just some unrealistically low value
+    minlosscount = 0
+    perm = False
+
+    model_best = deepcopy(model)  # Initialize before the training loop 
+
+    print('Starting training loop')
+    for epoch in tqdm(range(maxEpochs)):
+        #Set the learning rate annealing scheme 
+        lr = utils.cosineSGDR(optim, epoch, T0=Tcycle, eta_min=0, eta_max=lr0, scheme = 'constant')  
+        
+        model.train()
+        index_perm = np.arange(0, Ntrain, batch_size)
+        
+        if perm:
+            index_perm = np.random.permutation(index_perm)
+        epoch_losses = []
+        for index in index_perm:
+            inp, out = hf.loaddata_preloaded_train(index, batch_size, all_train_input, all_train_output, max_height-min_height, max_width-min_width)            
+            inp, out = totorch(inp), totorch(out)
+            out_mod = model(inp)
+            loss = criterion_train(out.squeeze(), out_mod.squeeze())
+            #Set gradient to zero
+            optim.zero_grad()
+            #Compute gradients       
+            loss.backward()
+            #Update parameters with new gradient
+            optim.step()
+            
+            epoch_losses.append(loss.item())
+            #Record train loss
+            #scheduler.step()
+        # Save average training loss for this epoch
+        train_loss_per_epoch[epoch] = np.mean(epoch_losses)
+        model.eval()
+        with torch.no_grad():
+            
+            out_mod=model(inp_test)
+            
+            r2 = R2(out_test.flatten(), (out_mod).cpu().numpy().flatten())
+            r2_test[epoch] = r2
+            #record current best model and best predictions
+            if maxR2 <  r2:
+                maxR2 = r2
+                epochmin.append(epoch)
+                maxr2l.append(maxR2)                
+                model_best = deepcopy(model)
+                corrs = []
+                pvals = []
+
+                # Ensure both arrays are on CPU and in numpy format
+                true_out = out_test
+                pred_out = (out_mod).cpu().numpy()  # already .cpu().numpy() earlier
+
+                for depth in range(true_out.shape[1]):
+                    true_flat = true_out[:, depth, :, :].flatten()
+                    pred_flat = pred_out[:, depth, :, :].flatten()
+    
+                    corr, pval = pearsonr(true_flat, pred_flat)
+                    corrs.append(corr)
+                    pvals.append(pval)
+
+                corr, pval = pearsonr(out_test.flatten(), pred_out.flatten())
+                print('R2:', r2, ' corr: ', corr, ' pval: ', pval)
+
+    print('Training finished')
+    model_best.eval()
+    with torch.no_grad():
+        model_best.to('cpu') 
+        out_mod = model_best(inp_test.to('cpu')).detach().cpu().numpy()
+        
+    print('Corr of best model:', pearsonr(out_test.flatten(), out_mod.flatten())[0])
+
+    Nx, Ny = out_test.shape[2:]; Nx, Ny
+
+    #save trained model
+    print(out_mod.shape, 'outout model shape')
+    dr = '/work/uo0780/u302044/project_unet_ehf/trained_models/vvel' 
+    try:
+        os.mkdir(dr)
+    except:
+        print('Directory exists or disk full...')
+    fstr = f'{save_fn_prefix}_{Nbase}_{lr0}_{batch_size}_{depth_levels}'
+    PATH = dr + f'/Debug_Unet_{fstr}_whole_year.pth'
+    torch.save({
+        'epoch': epochmin[-1] if epochmin else None,  # Best epoch
+        'model_state_dict': model_best.state_dict(),
+        'r2_test': r2_test,
+        'train_loss': train_loss_per_epoch,
+        'epochmin': epochmin,
+        'maxr2l': maxr2l,
+        'mean_input': mean_input,
+        'mean_output': mean_output,
+        'var_input': var_input,
+        'var_output': var_output,
+        'corrs': corrs,
+        'pvals': pvals,
+    }, PATH)
+
+nctrains, nctest = hf.load_data_from_nc_as_lists(vvel=True)
+
+# Count how many complete sets of three files are in the training data
+Ntrain = len(nctrains) // 3
+# Count how many complete sets of three files are in the testing data
+Ntest = len(nctest) // 3
+
+
+# Batch size calculation: the second biggest multiple of Ntrain is used 
+# (in case having enough memory one could use batch_size=Ntrain)
+
+multiples = [m for m in range(1, Ntrain+1) if Ntrain % m == 0]
+
+if len(multiples) < 2:
+    # If only one or zero multiples found, pick the biggest one or fallback to Ntrain
+    batch_size = multiples[-1] if multiples else Ntrain
+else:
+    # Pick the second biggest multiple
+    batch_size = multiples[-2]
+
+lr0 = 0.005*10/batch_size #maximum magnitude of the learning rate. Roughly should scale inversely to batch_size
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print ('Running on ', device)
+
+# region index calculation
+min_height, max_height, min_width, max_width = hf.convert_to_ij(lon_min, lon_max, lat_min, lat_max)
+
+#names of input variables, specific to the .nc files this code works on.
+var_names = [var_ssh, var_temp, var_output]
+var_input_names = [var_ssh, var_temp]
+var_output_names = [var_output]
+N_inp = len(var_input_names) 
+N_out = depth_levels
+
+save_fn_prefix  = 'any_{}{}_{}_nobatchnorm'.format(var_ssh, var_temp, var_output)
+    
+print('number of training records:', Ntrain)
+print('number of testing records:', Ntest)
+
+log_memory("Before loading data")
+all_train_input, all_train_output = hf.preload_data(nctrains, Ntrain, N_inp, N_out, var_input_names, depth_levels, var_output, min_height, max_height, min_width, max_width)
+all_test_input, all_test_output = hf.preload_data(nctest, Ntest, N_inp, N_out, var_input_names, depth_levels, var_output, min_height, max_height, min_width, max_width)
+log_memory("After loading data")
+print(all_train_input.shape, all_train_output.shape)
+
+# Data Normalization
+print("Computing Mean and Variance for Data Normalization")
+#Compute mean and variance for normalization
+mean_input = np.nanmean(all_train_input, axis=(0, 2, 3))
+mean_output = np.nanmean(all_train_output, axis=(0, 2, 3))
+
+all_train_input=all_train_input-mean_input[None, :, None, None]
+all_train_output=all_train_output-mean_output[None, :, None, None]
+all_test_input=all_test_input-mean_input[None, :, None, None]
+all_test_output=all_test_output-mean_output[None, :, None, None]
+
+var_input = np.nanmean(all_train_input**2, axis=(0, 2, 3))
+var_output = np.nanmean(all_train_output**2, axis=(0, 2, 3))
+
+print("Mean and variance of all input data:")
+print(mean_input,var_input)
+print("Mean and variance of all output data:")
+print(mean_output,var_output)
+
+# Set very small variances to avoid division by zero
+var_output = np.where(var_output == 0, 1e-9, var_output)
+
+#Scale the data so that they have variance of 
+all_train_input=all_train_input/np.sqrt(var_input[None, :, None, None])
+all_train_output=all_train_output/np.sqrt(var_output[None, :, None, None])
+all_test_input=all_test_input/np.sqrt(var_input[None, :, None, None])
+all_test_output=all_test_output/np.sqrt(var_output[None, :, None, None])
+
+run_model(var_input_names, var_output_names, save_fn_prefix, N_inp, N_out, mean_input, mean_output, var_input, var_output)
